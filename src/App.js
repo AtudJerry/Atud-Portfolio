c356ba4a373e28f2c3a8f6b5fbf5624929d92b1f
Atud Jerry <atudjerry33@gmail.com>
2023-04-29 18:13:22 +0100

    barchart containing skills added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Pic from './Components/Pic';
 import useMediaQuery from '@mui/material/useMediaQuery';
+import Skills from './Components/Skills';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Pic/>
-        <img src={logo} className="App-logo" alt="logo" />
+        <Skills/>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
